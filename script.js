e91a1b6ecfae7d40c60461f96737542d26e482f8
rafaelencinasr <rafaeladalbertoencinas94@gmail.com>
2022-07-19 17:55:20 -0700

    Prevent empty book creation, limit num. of pages
    Added a condition to the "Create book" button event listener, that
    checks if Title, Author, and Number of Pages inputs are empty. If any of
    this are empty, an alert pops up and reminds the user to finish filling
    the form or close it.
    
    Added a condition to the createBook prototype function that replaces the
    text displayed if the number of pages is bigger than:
    999999999999999999999999999999 but the value inside the object remains
    unchanged.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let myLibrary = [];
+let myLibraryTest = [];
 let i = 0;
+let bookIdNum = 0;
 const addButton = document.querySelector("#addButton");
 const container = document.querySelector('.bookContainer');
 const createBook = document.querySelector('#createBook');
@@ -13,22 +15,54 @@ function Book(){
     this.read   = document.getElementById('readStatus').checked;
 }
 
+/* const testObject0 = {
+    title: 'Title: "Harry Potter"',
+    author: 'Author: JK Simmons',
+    pages: 'Pages: 279',
+    read: 'Read',
+}
+
+const testObject1 = {
+    title: 'Title: "Test book2"',
+    author: 'Author: Testus Authorus',
+    pages: 'Pages: 123',
+    read: 'Unread',
+}
+
+myLibraryTest[0] = testObject0;
+myLibraryTest[1] = testObject1;
+ */
 Book.prototype.createCard = function(){
+
+    let parentContainer = document.querySelector('.bookContainer');
+
+    // Create the book container
     let div = document.createElement('div');
     div.classList.add('book');
-
+    div.dataset.bookId = bookIdNum;
+    
+    // Create book title element
     let bookTitle = document.createElement('div');
     bookTitle.classList.add('title');
     bookTitle.textContent = `Title: ${this.title}`;
 
+    // Create book author element
     let bookAuthor = document.createElement('div');
     bookAuthor.classList.add('author');
     bookAuthor.textContent = `Author: ${this.author}`;
 
+    // Create number of pages element
     let bookPages = document.createElement('div');
     bookPages.classList.add('pages');
-    bookPages.textContent = `No. of pages: ${this.pages}`;
+    if(this.pages>999999999999999999999999999999){
+        bookPages.textContent = `No. of pages: 999999999999999999999999999999`;
+    }
+    else{
+        bookPages.textContent = `No. of pages: ${this.pages}`;
+    }
+    
 
+    // Checks if book was read or not, assign text content accordingly 
     let readStatus = document.createElement('div');
     readStatus.classList.add('readStatus');
     if(this.read){
@@ -38,18 +72,47 @@ Book.prototype.createCard = function(){
         readStatus.textContent='Unread';
     }
 
+    // Creates a button that lets user change read status
+    let readStatusButton = document.createElement('button');
+    readStatusButton.classList.add('readStatusButton');
+    readStatusButton.textContent='Change status';
+    readStatusButton.addEventListener('click', ()=>{
+        this.read = !this.read;
+        if(this.read){
+            readStatus.textContent='Read';
+        }
+        else {
+            readStatus.textContent='Unread';
+        }
+    });
 
+    // Delete button creation
     let deleteBookIcon = document.createElement('button')
     deleteBookIcon.classList.add('deleteBook');
+    deleteBookIcon.dataset.bookId = bookIdNum;
+    // Adding event listener individually each time a new book is created
+    deleteBookIcon.addEventListener('click',()=>{
+        //Search for an object in the myLibrary array that matches the book title and get the index
+        let delIndex = myLibrary.findIndex((book)=>book.title==this.title);
+        //Delete the object at the found index
+        myLibrary.slice(delIndex,1);
+        //Delete element from the DOM
+        parentContainer.removeChild(div);
+    });
+
+    // Creates the trash icon from the URL
     let trashIcon = document.createElement('img');
     trashIcon.src= './img/delete.png';
 
+    // Changes book ID for next book attr.
+    bookIdNum++;
     deleteBookIcon.appendChild(trashIcon);
 
     div.appendChild(bookTitle);
     div.appendChild(bookAuthor);
     div.appendChild(bookPages);
     div.appendChild(readStatus);
+    div.appendChild(readStatusButton);
     div.appendChild(deleteBookIcon);
     container.appendChild(div);
 
@@ -61,25 +124,65 @@ Book.prototype.createCard = function(){
     console.log(searchIndex);
 }  */
 
+/* Book.prototype.createDeleteBtnListener = function(){
+    let  
+}
+ */
+let emptyFlag = true;
+
 createBook.addEventListener('click', ()=>{
-    myLibrary[i] = new Book();
+    if(document.getElementById('bookTitle').value && document.getElementById('bookAuthor').value && document.getElementById('numPages').value){
+        emptyFlag = false;
+        console.log("We have a title")
+        myLibrary[i] = new Book();
+        console.log(myLibrary[i].title);
+        myLibrary[i].createCard();
+        // myLibrary[i].createDeleteBtnListener
+        i++;
+        document.getElementById("popupForm").style.display='none';
+    }
+    else{
+        emptyFlag = true;
+        console.log('SOmething left empty :(')
+        alert("Please fill all the information or close the form");
+    }
+/*     myLibrary[i] = new Book();
     console.log(myLibrary[i].title);
     myLibrary[i].createCard();
-    i = i+1;
-    
+    // myLibrary[i].createDeleteBtnListener
+    i++;
+     */
 
-    deleteBookButton.forEach(button=>button.removeEventListener('click', testDeleteButton));
+/*     deleteBookButton.forEach(button=>button.removeEventListener('click', testDeleteButton));
     deleteBookButton = document.querySelectorAll('.deleteBook');
-    deleteBookButton.forEach(button=>button.addEventListener('click', testDeleteButton));
-    document.getElementById("popupForm").style.display='none';
+    deleteBookButton.forEach(button=>button.addEventListener('click', testDeleteButton)); */
+    
 });
 
-function testDeleteButton(){
+ function testDeleteButton(){
     console.log("Del button");
-};
+}; 
 
-let deleteBookButton = document.querySelectorAll('.deleteBook');
-deleteBookButton.forEach(button=>button.addEventListener('click', testDeleteButton));
+/* let deleteBookButton = document.querySelectorAll('.deleteBook');
+deleteBookButton.forEach(button=>button.addEventListener('click', testDeleteButton)); */
+
+
+/* let testattVar = 'testatt2';        //Attribute we want to know the element of
+let testAttributeQuery = document.querySelector(`[data-test = ${testattVar}]`).innerHTML; //getting the element text content
+console.log(testAttributeQuery);
+let searchIndex = myLibraryTest.findIndex((book)=>book.title==testAttributeQuery); // Search the object array
+console.log(`Library index: ${searchIndex}`);
+console.log(myLibraryTest[searchIndex]);
+ */
+
+/* let testValue = testAttributeQuery.value;
+console.log(testValue); */
+
+/* let deleteBookButton = document.querySelector('.deleteBook');
+let deleteId = deleteBookButton.dataset.test;
+deleteBookButton.addEventListener('click', ()=>{
+    console.log(deleteId);
+}); */
 
 addButton.addEventListener('click',()=>{
     clearForm();
@@ -97,6 +200,6 @@ function clearForm(){
     document.getElementById('bookTitle').value = '';
     document.getElementById('bookAuthor').value = '';
     document.getElementById('numPages').value = '';
-    document.getElementById('readStatus').checked;
+    document.getElementById('readStatus').checked = false;
 }
 
